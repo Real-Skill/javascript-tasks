dbcfad228ea4aa97feae8dd88bdc0b7655ab8156
aniaw <ania_w@outlook.com>
2015-03-17 20:58:52 +0100

    standarization

--- app/Filter.js
+++ app/Filter.js
@@ -1,31 +1,41 @@
-var myApp = angular.module('myApp', []);
+'use strict';
+var app = angular.module('app', []);
 
-myApp.filter('quote', function () {
-    return function (text) {
+app.filter('quote', function ()
+{
+    return function (text)
+    {
         return '"' + text + '"';
-    }
+    };
 });
 
-myApp.filter('withoutH', function () {
-    return function (text) {
-        return text.split('h').join("").split('H').join("");
-    }
+app.filter('withoutH', function ()
+{
+    return function (text)
+    {
+        return text.split('h').join('').split('H').join('');
+    };
 });
 
-myApp.filter('firstLetterUpp', function () {
-    return function (text) {
-        return text.replace(/([^\W_]+[^\s-]*) */g, function (text) {
+app.filter('firstLetterUpp', function ()
+{
+    return function (text)
+    {
+        return text.replace(/([^\W_]+[^\s-]*) */g, function (text)
+        {
             return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
         });
-    }
+    };
 });
 
 
-function FilterCtrl($scope) {
+app.controller('FilterCtrl', function ($scope)
+{
     $scope.data = {
-        citation: "To be, or not to be...",
-        title: "The Hobbit: The Battle of the Five Armies",
-        someText: "Visit BBC News for up-to-the-minute news, breaking news, video, audio and stories."
-    }
+        citation: 'To be, or not to be...',
+        title: 'The Hobbit: The Battle of the Five Armies',
+        someText: 'Visit BBC News for up-to-the-minute news, breaking news, video, audio and stories.'
+    };
 
-}
+});
+
