db37de0bfa8f0c203d68e62bcbdd6a4012712a7d
Szymon Maslowski <szymon.maslowski@vp.pl>
2015-05-26 00:40:08 +0200

    without answer

--- app/DAO/phoneDAO.js
+++ app/DAO/phoneDAO.js
@@ -13,28 +13,7 @@
 
     function search(query)
     {
-        var
-            skip = 0,
-            limit = query.limit ? parseInt( query.limit, 10 ) : 2,
-            total;
 
-        if( query.hasOwnProperty( 'skip' ) && query.skip > 0 ) {
-            skip = parseInt( query.skip, 10 );
-        }
-        if( query.hasOwnProperty( 'limit' ) && query.skip > -1 ) {
-            skip = parseInt( query.skip, 10 );
-        }
-
-        return Model.find().count().then( function( data ) {
-            total = data;
-            return Model.find()
-                .skip( skip )
-                .limit( limit )
-                .execQ()
-                .then( function( data ) {
-                    return { results: data, total: total };
-                } );
-        });
     }
 
     module.exports = {
