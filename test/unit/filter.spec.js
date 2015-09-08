11103b8f10de0fcc851ed67025856f2a067e6738
aniaw <ania_w@outlook.com>
2015-09-08 08:30:16 +0200

    configuration complete

--- test/unit/filter.spec.js
+++ test/unit/filter.spec.js
@@ -11,6 +11,10 @@ describe('filter', function ()
         it('should places the text in quotes', inject(function (quoteFilter)
         {
             expect(quoteFilter('To be, or not to be...')).toEqual('"To be, or not to be..."');
+        }));
+
+        it('should put text in quotes', inject(function (quoteFilter)
+        {
             expect(quoteFilter('Test test')).toEqual('"Test test"');
         }));
     });
@@ -38,11 +42,16 @@ describe('filter', function ()
 
     describe('firstLetterUpp', function ()
     {
-        it('should places the text in quotes', inject(function (firstLetterUpFilter)
+        it('should change each word first letter uppercase', inject(function (firstLetterUpFilter)
         {
-            expect(firstLetterUpFilter('Up-to-the-minute')).toEqual('Up-To-The-Minute');
             expect(firstLetterUpFilter('the Hobbit: the Battle of the five Armies')).toEqual('The Hobbit: The Battle Of The Five Armies');
+        }));
+        it('should change first letter uppercase', inject(function (firstLetterUpFilter)
+        {
             expect(firstLetterUpFilter('first letter up')).toEqual('First Letter Up');
+        }));
+        it('should change first letter uppercase for separated by -', inject(function (firstLetterUpFilter)
+        {
             expect(firstLetterUpFilter('Visit BBC News for up-to-the-minute news, breaking news, video, audio and stories.')).toEqual('Visit Bbc News For Up-To-The-Minute News, Breaking News, Video, Audio And Stories.');
         }));
     });
