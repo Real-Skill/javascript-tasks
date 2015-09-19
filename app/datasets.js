f06da91bf3e00ea2e726a83b20a716f7d5483192
Bernard Labno <s4237@pjwstk.edu.pl>
2015-09-19 11:22:33 +0200

    Initial commit

---
+++ app/datasets.js
@@ -0,0 +1,315 @@
+'use strict';
+
+module.exports = {
+
+    assign: function ()
+    {
+        return [];
+    },
+
+    //alias _.extend
+    assignIn: function ()
+    {
+        return [];
+    },
+
+    at1: function (obj)
+    {
+        return [];
+    },
+
+    at2: function ()
+    {
+        return [];
+    },
+
+    defaults: function ()
+    {
+        return [];
+    },
+
+    defaultsDeep: function ()
+    {
+        return [];
+    },
+
+    findKey1: function (obj)
+    {
+        return [];
+    },
+
+    findKey2: function (obj)
+    {
+        return [];
+    },
+
+    findKey3: function (obj)
+    {
+        return [];
+    },
+
+    findKey4: function (obj)
+    {
+        return [];
+    },
+
+    findLastKey1: function (obj)
+    {
+        return [];
+    },
+
+    findLastKey2: function (obj)
+    {
+        return [];
+    },
+
+    findLastKey3: function (obj)
+    {
+        return [];
+    },
+
+    findLastKey4: function (obj)
+    {
+        return [];
+    },
+
+    forIn: function ()
+    {
+        return [];
+    },
+
+    forInRight: function ()
+    {
+        return [];
+    },
+
+    forOwn: function ()
+    {
+        return [];
+    },
+
+    forOwnRight: function ()
+    {
+        return [];
+    },
+
+    functions: function ()
+    {
+        return [];
+    },
+
+    functionsIn: function ()
+    {
+        return [];
+    },
+
+    get1: function (obj)
+    {
+        return [];
+    },
+
+    get2: function (obj)
+    {
+        return [];
+    },
+
+    get3: function (obj)
+    {
+        return [];
+    },
+
+    has1: function ()
+    {
+        return [];
+    },
+
+    has2: function ()
+    {
+        return [];
+    },
+
+    hasIn1: function ()
+    {
+        return [];
+    },
+
+    hasIn2: function ()
+    {
+        return [];
+    },
+
+    invert: function (obj1)
+    {
+        return [];
+    },
+
+    invertBy1: function (obj1)
+    {
+        return [];
+    },
+
+    invertBy2: function (obj)
+    {
+        return [];
+    },
+
+    invoke1: function (obj)
+    {
+        return [];
+    },
+
+    invoke2: function (obj)
+    {
+        return [];
+    },
+
+    keys: function ()
+    {
+        return [];
+    },
+
+    keysIn: function ()
+    {
+        return [];
+    },
+
+    mapKeys2: function (obj)
+    {
+        return [];
+    },
+
+    mapValues1: function (obj)
+    {
+        return [];
+    },
+
+    mapValues2: function (obj)
+    {
+        return [];
+    },
+
+    merge: function ()
+    {
+        return [];
+    },
+
+    mergeWith: function ()
+    {
+        return [];
+    },
+
+    omit1: function (obj)
+    {
+        return [];
+    },
+
+    omit2: function (obj)
+    {
+        return [];
+    },
+
+    omitBy: function (obj, filter)
+    {
+        return [];
+    },
+
+    pick1: function ()
+    {
+        return [];
+    },
+
+    pick2: function ()
+    {
+        return [];
+    },
+
+    pickBy: function ()
+    {
+        return [];
+    },
+
+    result1: function (obj)
+    {
+        return [];
+    },
+
+    result2: function (obj)
+    {
+        return [];
+    },
+
+    result3: function (obj)
+    {
+        return [];
+    },
+
+    result4: function (obj)
+    {
+        return [];
+    },
+
+    set1: function ()
+    {
+        return [];
+    },
+
+    set2: function ()
+    {
+        return [];
+    },
+
+    setWith: function ()
+    {
+        return [];
+    },
+
+    toPairs: function ()
+    {
+        return [];
+    },
+
+    toPairsIn: function ()
+    {
+        return [];
+    },
+
+    transform1: function ()
+    {
+        return [];
+    },
+
+    transform2: function ()
+    {
+        return [];
+    },
+
+    unset1: function ()
+    {
+        return [];
+    },
+
+    unset2: function ()
+    {
+        return [];
+    },
+
+    update: function ()
+    {
+        return [];
+    },
+
+    updateWith: function ()
+    {
+        return [];
+    },
+
+    values: function ()
+    {
+        return [];
+    },
+
+    valuesIn: function ()
+    {
+        return [];
+    }
+};
