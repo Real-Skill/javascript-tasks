2ef836b11a901f0272e3806dd927b9fcb71a69ea
Bernard Labno <s4237@pjwstk.edu.pl>
2015-09-19 11:22:33 +0200

    lodash Util tests

---
+++ app/datasets.js
@@ -0,0 +1,117 @@
+'use strict';
+
+
+module.exports = {
+
+    attempt: function(){
+        return[];
+    },
+
+    identity: function(){
+        return[];
+    },
+
+    method: function() {
+        return[];
+    },
+
+    methodOf: function() {
+        return[];
+    },
+
+    noop: function(){
+        return[];
+    },
+
+    nthArg1: function(){
+        return[];
+    },
+
+    nthArg2: function(){
+        return[];
+    },
+
+    over1: function(){
+        return[];
+    },
+
+    over2: function(){
+        return[];
+    },
+
+    over3: function(){
+        return[];
+    },
+
+    overEvery1: function(){
+        return[];
+    },
+
+    overEvery2: function(){
+        return[];
+    },
+
+    overSome1: function(){
+        return[];
+    },
+
+    overSome2: function(){
+        return[];
+    },
+
+    property1: function(){
+        return[];
+    },
+
+    property2: function(){
+        return[];
+    },
+
+    propertyOf1: function(){
+        return[];
+    },
+
+    propertyOf2: function(){
+        return[];
+    },
+
+    range1: function(){
+        return[];
+    },
+
+    range2: function(){
+        return[];
+    },
+
+    range3: function(){
+        return[];
+    },
+
+    rangeRight1: function(){
+        return[];
+    },
+
+    rangeRight2: function(){
+        return[];
+    },
+
+    rangeRight3: function(){
+        return[];
+    },
+
+    times: function(){
+        return[];
+    },
+
+    toPath: function(){
+        return[];
+    },
+
+    uniqueId1: function(){
+        return[];
+    },
+
+    uniqueId2: function(){
+        return[];
+    }
+};
