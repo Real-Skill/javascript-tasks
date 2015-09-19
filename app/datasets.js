8a375c5706c4586c5eb1598390bbba9dd499558d
Bernard Labno <s4237@pjwstk.edu.pl>
2015-09-19 11:22:33 +0200

    Add test for lodash-number

---
+++ app/datasets.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+
+    clamp1: function ()
+    {
+        return [];
+    },
+
+    clamp2: function ()
+    {
+        return [];
+    },
+
+    clamp3: function ()
+    {
+        return [];
+    },
+
+    inRange1: function ()
+    {
+        return [];
+    },
+
+    inRange2: function ()
+    {
+        return [];
+    },
+
+    inRange3: function ()
+    {
+        return [];
+    },
+
+    inRange4: function ()
+    {
+        return [];
+    },
+
+    inRange5: function ()
+    {
+        return [];
+    },
+
+    inRange6: function ()
+    {
+        return [];
+    },
+
+    inRange7: function ()
+    {
+        return [];
+    },
+
+    random1: function ()
+    {
+        return [];
+    },
+    random2: function ()
+    {
+        return [];
+    }
+};
