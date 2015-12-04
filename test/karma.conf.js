bfd59574856e096fbfc4b7011a989e67ffdbeb17
aniaw <ania_w@outlook.com>
2015-12-04 09:00:45 +0100

    update package list

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config)
         frameworks: ['jasmine'],
 
         // list of files / patterns to load in the browser
-        files: ['node_modules/sinon/pkg/sinon.js','app/**/*.js','test/unit/**/*.spec.js'],
+        files: ['app/**/*.js','test/unit/**/*.spec.js'],
 
         // list of files / patterns to exclude
         exclude: [],
