0ce870d0bd4ce64fce6c48f893904b30b52634f6
aniaw <ania_w@outlook.com>
2015-12-07 12:31:20 +0100

    update scaffolding

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,11 +2,18 @@
 {
     'use strict';
 
+    var Promise = require('bluebird');
+    var chai = require('chai');
+    var expect = chai.expect;
+    var sinon = require('sinon');
+    chai.use(require('sinon-chai'));
     var app = require('../../app/app');
+    Promise.longStackTraces();
+
 
     describe('app', function ()
     {
-        it('should ...', function ()
+        it('should...', function ()
         {
 
         });
