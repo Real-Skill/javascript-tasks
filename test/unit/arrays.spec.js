0c20de8a3eda0983e3a04bca24328c9c400db348
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2015-12-14 18:54:00 +0100

    Better tests

--- test/unit/arrays.spec.js
+++ test/unit/arrays.spec.js
@@ -5,124 +5,151 @@
 
     describe('arrays', function ()
     {
-        var a;
-
-        beforeEach(function ()
-        {
-            a = [1, 2, 3, 4];
-        });
-
         it('you should be able to determine the location of an item in an array', function ()
         {
-            expect(arraysAnswers.indexOf(a, 3)).toBe(2);
-            expect(arraysAnswers.indexOf(a, 5)).toBe(-1);
+            expect(arraysAnswers.indexOf([1, 2, 3, 4], 3)).toBe(2);
+            expect(arraysAnswers.indexOf([1, 2, 3, 4], 5)).toBe(-1);
         });
 
         it('you should be able to add the values of an array', function ()
         {
-            expect(arraysAnswers.sum(a)).toBe(10);
+            expect(arraysAnswers.sum([1, 2, 3, 4])).toBe(10);
+            expect(arraysAnswers.sum([1, 3, 5, 7, 9])).toBe(1 + 3 + 5 + 7 + 9);
+            expect(arraysAnswers.sum([18, 27, 33])).toBe(18 + 27 + 33);
+        });
+
+        it('you should be able to return a copy of an array without given value', function ()
+        {
+            var input;
+
+            input = [1, 2, 3, 4, 2];
+            expect(arraysAnswers.remove(input, 2)).toEqual([1, 3, 4]);
+            expect(input).toEqual([1, 2, 3, 4, 2]);
+
+            input = [1, 2, 3, 4, 2];
+            expect(arraysAnswers.remove(input, 3)).toEqual([1, 2, 4, 2]);
+            expect(input).toEqual([1, 2, 3, 4, 2]);
+
+
+            input = [5, 5, 5, 5, 5, 5];
+            expect(arraysAnswers.remove(input, 5)).toEqual([]);
+            expect(input).toEqual([5, 5, 5, 5, 5, 5]);
         });
 
         it('you should be able to remove all instances of a value from an array', function ()
         {
-            a.push(2); // Make sure the value appears more than one time
-            var result = arraysAnswers.remove(a, 2);
+            function doTest(array, item, expectedResult)
+            {
+                arraysAnswers.removeWithoutCopy(array, item);
+                expect(array).toEqual(expectedResult);
+            }
 
-            expect(result.length).toBe(3);
-            expect(result.join(' ')).toBe('1 3 4');
-        });
-
-        it('you should be able to remove all instances of a value from an array, returning the original array', function ()
-        {
-            a.splice(1, 0, 2);
-            a.push(2);
-            a.push(2);
-
-            var result = arraysAnswers.removeWithoutCopy(a, 2);
-
-            expect(result.length).toBe(3);
-            expect(result.join(' ')).toBe('1 3 4');
-
-            // make sure that you return the same array instance
-            expect(result).toEqual(a);
+            doTest([1, 2, 3, 4, 2], 2, [1, 3, 4]);
+            doTest([1, 2, 3, 4, 2], 3, [1, 2, 4, 2]);
+            doTest([5, 5, 5, 5, 5, 5], 5, []);
         });
 
         it('you should be able to add an item to the end of an array', function ()
         {
-            var result = arraysAnswers.append(a, 10);
+            var input = [1, 6, 8];
+            arraysAnswers.append(input, 10);
+            expect(input).toEqual([1, 6, 8, 10]);
 
-            expect(result.length).toBe(5);
-            expect(result[result.length - 1]).toBe(10);
+            input = [];
+            arraysAnswers.append(input, 2);
+            expect(input).toEqual([2]);
+
+            input = [3, 3];
+            arraysAnswers.append(input, 3);
+            expect(input).toEqual([3, 3, 3]);
+        });
+
+        it('you should be able to add an item to the end of an array without modifying original array', function ()
+        {
+            var input = [1, 6, 8];
+            expect(arraysAnswers.appendToCopy(input, 10)).toEqual([1, 6, 8, 10]);
+            expect(input).toEqual([1, 6, 8]);
+
+            input = [];
+            expect(arraysAnswers.appendToCopy(input, 2)).toEqual([2]);
+            expect(input).toEqual([]);
+
+            input = [3, 3];
+            expect(arraysAnswers.appendToCopy(input, 3)).toEqual([3, 3, 3]);
+            expect(input).toEqual([3, 3]);
         });
 
         it('you should be able to remove the last item of an array', function ()
         {
-            var result = arraysAnswers.truncate(a);
-
-            expect(result.length).toBe(3);
-            expect(result.join(' ')).toBe('1 2 3');
+            expect(arraysAnswers.truncate([1, 2, 3, 4])).toEqual([1, 2, 3]);
+            expect(arraysAnswers.truncate([1])).toEqual([]);
+            expect(arraysAnswers.truncate([])).toEqual([]);
         });
 
         it('you should be able to add an item to the beginning of an array', function ()
         {
-            var result = arraysAnswers.prepend(a, 10);
-
-            expect(result.length).toBe(5);
-            expect(result[0]).toBe(10);
+            expect(arraysAnswers.prepend([1, 2, 3, 4], 5)).toEqual([5, 1, 2, 3, 4]);
+            expect(arraysAnswers.prepend([1], 3)).toEqual([3, 1]);
+            expect(arraysAnswers.prepend([], 2)).toEqual([2]);
         });
 
         it('you should be able to remove the first item of an array', function ()
         {
-            var result = arraysAnswers.curtail(a);
-
-            expect(result.length).toBe(3);
-            expect(result.join(' ')).toBe('2 3 4');
+            expect(arraysAnswers.curtail([1, 2, 3, 4])).toEqual([2, 3, 4]);
+            expect(arraysAnswers.curtail([8, 54, 2])).toEqual([54, 2]);
         });
 
         it('you should be able to join together two arrays', function ()
         {
-            var c = ['a', 'b', 'c', 1];
-            var result = arraysAnswers.concat(a, c);
-
-            expect(result.length).toBe(8);
-            expect(result.join(' ')).toBe('1 2 3 4 a b c 1');
+            expect(arraysAnswers.concat([1, 2, 3, 4], ['a', 'b', 'c', 1])).toEqual([1, 2, 3, 4, 'a', 'b', 'c', 1]);
+            expect(arraysAnswers.concat([], [])).toEqual([]);
         });
 
         it('you should be able to add an item anywhere in an array', function ()
         {
-            var result = arraysAnswers.insert(a, 'z', 2);
+            var input = [1, 2, 3, 4];
+            arraysAnswers.insert(input, 'z', 2);
+            expect(input).toEqual([1, 2, 'z', 3, 4]);
 
-            expect(result.length).toBe(5);
-            expect(result.join(' ')).toBe('1 2 z 3 4');
+
+            input = [];
+            arraysAnswers.insert(input, 'c', 3);
+            expect(input).toEqual(['c']);
+
+            input = [];
+            arraysAnswers.insert(input, 'x', 0);
+            expect(input).toEqual(['x']);
         });
 
         it('you should be able to count the occurences of an item in an array', function ()
         {
-            var result = arraysAnswers.count([1, 2, 4, 4, 3, 4, 3], 4);
-
-            expect(result).toBe(3);
+            expect(arraysAnswers.count([1, 2, 4, 4, 3, 4, 3], 4)).toBe(3);
+            expect(arraysAnswers.count([1, 1], 1)).toBe(2);
+            expect(arraysAnswers.count([1, 1], 2)).toBe(0);
+            expect(arraysAnswers.count([], 1)).toBe(0);
         });
 
         it('you should be able to find duplicates in an array', function ()
         {
-            var result = arraysAnswers.duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3]);
-
-            expect(result.sort()).toEqual([1, 3, 4]);
+            expect(arraysAnswers.duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3]).sort()).toEqual([1, 3, 4]);
+            expect(arraysAnswers.duplicates([5, 7, 2]).sort()).toEqual([]);
+            expect(arraysAnswers.duplicates([5, 7, 2, 2, 2, 7, 5]).sort()).toEqual([2, 5, 7]);
+            var object = {};
+            expect(arraysAnswers.duplicates([object, object, 2, object, 7, 2, 5]).sort()).toEqual([2, object]);
         });
 
         it('you should be able to square each number in an array', function ()
         {
-            var result = arraysAnswers.square(a);
-
-            expect(result.length).toBe(4);
-            expect(result.join(' ')).toBe('1 4 9 16');
+            expect(arraysAnswers.square([1, 2, 3, 4])).toEqual([1, 4, 9, 16]);
+            expect(arraysAnswers.square([7, 7, 7])).toEqual([49, 49, 49]);
+            expect(arraysAnswers.square([4, 3, 2])).toEqual([16, 9, 4]);
         });
 
         it('you should be able to find all occurrences of an item in an array', function ()
         {
-            var result = arraysAnswers.findAllOccurrences('abcdefabc'.split(''), 'a');
-
-            expect(result.sort().join(' ')).toBe('0 6');
+            expect(arraysAnswers.findAllOccurrences('abcdefabc'.split(''), 'a')).toEqual([0, 6]);
+            expect(arraysAnswers.findAllOccurrences('abcdefabc'.split(''), 'z')).toEqual([]);
+            expect(arraysAnswers.findAllOccurrences('abcdefabc'.split(''), 'b')).toEqual([1, 7]);
         });
     });
 })();
