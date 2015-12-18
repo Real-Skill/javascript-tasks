3b82fe1e93c28f78993c01e8783e7423f9397d71
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2015-12-18 13:54:51 +0100

    cleanup

--- app/datasets.js
+++ app/datasets.js
@@ -3,84 +3,58 @@
 module.exports = {
     chunk: function ()
     {
-        var array = [1, 2, 3, 4, 5, 6, 7, 8];
-        var size = 3;
-        return [array, size];
+        return [];
     },
     chunk2: function ()
     {
-        var array = [1, 2, 3, 4, 5, 6, 7, 8];
-        var size = 2;
-        return [array, size];
+        return [];
     },
     compact: function ()
     {
-        var array = [1, null, false, '', 0, 2, undefined, 3];
-        return [array];
+        return [];
     },
     difference1: function ()
     {
-        var baseArray = [1, null, false, '', 0, 2, undefined, 3];
-        var exclusion1 = [1, '', 0];
-        var exclusion2 = [null, false, 2, undefined, 3];
-        return [baseArray, exclusion1, exclusion2];
+        return [];
     },
     difference2: function ()
     {
-        var baseArray = [1, null, false, '', 0, 2, undefined, 3];
-        var exclusion1 = [1, '', 0];
-        return [baseArray, exclusion1];
+        return [];
     },
     drop1: function ()
     {
-        var array = [1, 2, {}, new Array(5)];
-        return [array];
+        return [];
     },
     drop2: function ()
     {
-        var array = [1, 2, {}, new Array(5)];
-        var numberOfElements = 2;
-        return [array, numberOfElements];
+        return [];
     },
     dropRight: function ()
     {
-        var array = [1, 2, {}, new Array(5)];
-        var numberOfElements = 3;
-        return [array, numberOfElements];
+        return [];
     },
     fill1: function ()
     {
-        var array = new Array(110);
-        return [array, '*', 3, 103];
+        return [];
     },
     fill2: function ()
     {
-        var array = new Array(1000);
-        return [array, 'donkey'];
+        return [];
     },
     findIndex1: function (array)
     {
-        function predicate(item)
-        {
-            return 'Jack' === item.getName();
-        }
-
-        return [array, predicate];
+        return [];
     },
     findIndex2: function ()
     {
-        var array = [1, {name: 'Jack', age: 33}, {name: 'Rick', age: 21}, 21, {name: 'Jack', age: 21}, 4, 5];
-        var predicate = {name: 'Jack', age: 21};
-        return [array, predicate];
+        return [];
     },
     first1: function ()
     {
-        var array = [];
-        return [array];
+        return [];
     },
     first2: function ()
     {
-        var array = [1];
-        return [array];
+        return [];
     }
 };
