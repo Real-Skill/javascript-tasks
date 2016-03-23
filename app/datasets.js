99d108541860873e332ae0de792f12a1bbf47316
TomekS <tomaszstysial@o2.pl>
2016-03-23 15:26:33 +0100

    ADD some functions

--- app/datasets.js
+++ app/datasets.js
@@ -4,5 +4,28 @@ module.exports = {
     after: function (callback)
     {
         return [2, callback];
+    },
+    bind: function (callback) {
+        var obj = {order: 'PIZZA'};
+        return [callback, obj, 'YES'];
+    },
+    delay: function (callback) {
+
+        return [ callback, 1000,'asd'];
+    },
+    flip: function () {
+        var testFunc = function () {
+            var arr = [];
+            for(var key in arguments) {
+                if(arguments.hasOwnProperty(key)) {
+                    arr[key] = arguments[key];
+                }
+            }
+            return arr;
+        };
+        return [testFunc];
+    },
+    overArgs: function (callback) {
+        return [callback];
     }
 };
--- test/unit/lodash.spec.js
+++ test/unit/lodash.spec.js
@@ -11,7 +11,7 @@ var sinon = require('sinon');
 describe('Lodash Functions training', function ()
 {
     describe('after', function () {
-        it('should', function () {
+        it('should invoke the function after second call', function () {
             var spy = sinon.spy();
             var params = datasets.after(spy);
 
@@ -22,6 +22,69 @@ describe('Lodash Functions training', function ()
             expect(spy).to.have.been.callCount(1);
             after();
             expect(spy).to.have.been.callCount(2);
+        });
+    });
+
+    describe('bind', function () {
+        it('should bind function with object and argument "YES"', function () {
+            var testFunc = function (complete) {
+                return this.order + ' is ready: ' + complete;
+            };
+            var spy = sinon.spy(testFunc);
+            var params = datasets.bind(spy);
+            var bind = _.bind.apply(_, params);
+
+            bind();
+            expect(spy).to.have.been.callCount(1);
+            expect(spy).to.have.been.calledWith('YES');
+            expect(spy).to.have.been.returned('PIZZA is ready: YES');
+        });
+    });
+
+    //describe.only('curry', function () {
+    //    it('should', function () {
+    //        var abc = function (a,b,c) {
+    //            return a+b+c;
+    //        };
+    //
+    //        var params = datasets.curry();
+    //        var result = _.curry(abc);
+    //        expect(_.curry.apply(_, params)).to.equal(result);
+    //    });
+    //});
+    //
+    //describe.only('delay', function () {
+    //    it('should invoke function after 3 seconds', function () {
+    //        var testFunc = function (x) {
+    //            console.log(x);
+    //        };
+    //        var spy = sinon.spy(testFunc);
+    //        var params = datasets.delay(spy);
+    //        var delay = _.delay.apply(_, params);
+    //
+    //        delay();
+    //        expect(spy).to.have.been.callCount(1);
+    //
+    //    });
+    //});
+
+    describe('flip', function () {
+        it('should return array with flipped arguments', function () {
+            var params = datasets.flip();
+            var flip = _.flip.apply(_,params);
+            var result = flip(1,2,3,4,5,6);
+            var argumentType = typeof params[0];
+            expect(argumentType).to.eql('function');
+            expect(result).to.eql([6,5,4,3,2,1]);
+            result = flip('a','b','c','d');
+            expect(result).to.eql(['d','c','b','a']);
+
+        });
+    });
+
+    describe('overArgs',function () {
+        it('should smthg', function () {
+
         });
     });
 });
