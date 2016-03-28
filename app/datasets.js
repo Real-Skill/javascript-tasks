964352334938b6345c61869ed4ee85f48aecd296
stychu <tomaszstysial@o2.pl>
2016-03-28 12:19:39 +0200

    some tests added, they need to be checked

--- app/datasets.js
+++ app/datasets.js
@@ -3,49 +3,30 @@
 module.exports = {
     after: function (callback)
     {
-        return [2, callback];
+        return [];
     },
     bind: function (callback) {
-        var obj = {order: 'PIZZA'};
-        return [callback, obj, 'YES'];
+
+        return [];
     },
     curry: function () {
-        var cube = function (x) {
-          return x * x * x;
-        };
 
-        return [cube];
+        return [];
     },
     flip: function () {
-        var testFunc = function () {
-            var arr = [];
-            for(var key in arguments) {
-                if(arguments.hasOwnProperty(key)) {
-                    arr[key] = arguments[key];
-                }
-            }
-            return arr;
-        };
-        return [testFunc];
+
+        return [];
     },
     partial: function (callback) {
-        return [callback, 'Batman', 'GOOD'];
+
+        return [];
     },
     rearg: function (callback) {
-        return [callback, 1, 3, 0, 2];
+
+        return [];
     },
     spread: function () {
-        var action = function (virus, treatment) {
-            if (!virus || !virus && !treatment) { return false;}
-            if (treatment === 'started') {
-                return 'Treatment due to ' + virus + ' virus has started!';
-            } else if (treatment === "some") {
-                return virus + ' slowly killing you, I\'m\ so sorry';
-            } else {
-                return 'You were killed by ' + virus;
-            }
-        };
 
-        return [action];
+        return [];
     }
 };
